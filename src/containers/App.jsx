32212d5fc0e5678874941958db31422184c2b4e4
Marco Mora <mmoracascante@gmail.com>
2020-05-07 23:39:26 -0600

    PlatziVideo

---
+++ src/containers/App.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import Header from '../components/Header';
+import '../assets/styles/App.scss';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
+
+const API = 'http://localhost:3000/initalState';
+
+const App = () => {
+  const initalState = useInitialState(API);
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+      {initalState.mylist.length > 0 && (
+        <Categories title='Mi lista'>
+          <Carousel>
+            {initalState.mylist.map((item) => <CarouselItem key={item.id} {...item} />)}
+          </Carousel>
+        </Categories>
+      ) }
+      <Categories title='Tendencias'>
+        <Carousel>
+          {initalState.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+      <Categories title='Originales de Platzi Video'>
+        <Carousel>
+          {initalState.originals.map((item) => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+      <Footer />
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const useInitialState = () => {
+  const [videos, setVideos] = useState(
+    {
+      'mylist': [],
+      'trends': [],
+      'originals': [],
+    },
+  );
+  useEffect(() => {
+    fetch('http://localhost:3000/initalState')
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+  return videos;
+};
+
+export default useInitialState;
